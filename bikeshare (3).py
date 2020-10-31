5a22c46cad96eb00db7171ba9da496e940dac13e
Sinem-Sena-Kisacik <sinem.kisacik@turkcell.com.tr>
2020-10-31 18:40:47 +0300

    refactor: changes made in bikeshare made changes in line 73

--- bikeshare (3).py
+++ bikeshare (3).py
@@ -70,7 +70,7 @@ def load_data(city, month, day):
     Loads data for the specified city and filters by month and day if applicable.
     Args:
          (str): name of the city to analyze
-         (str): name of the month to filter by, or "all" to apply no month filter
+         (str): name of the month 
          (str): name of the day of week to filter by, or "all" to apply no day filter
     Returns:
         df: Pandas DataFrame containing city data filtered by month and day
